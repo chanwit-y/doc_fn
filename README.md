599f333931cdd7a7719b148dea520e8e6f44d872
cy <chanwit.yimneam@hotmail.com>
2026-04-07 14:06:24 +0700

    Update diesel.toml file path, enhance README with task list, and remove user table migration files
    - Changed the file path in diesel.toml for schema generation.
    - Expanded README.md to include a task list for project organization.
    - Deleted user table migration files (up.sql and down.sql) as they are no longer needed.
    - Updated schema mod.rs to reflect the latest database structure.

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 cargo install diesel_cli --no-default-features --features postgres
 diesel migration run
 ```
+
+task
+- config logic app
+- listener
+- move file and insert file info into database
+- databricks function
+   - upload file
+   - get data from table
+- check file info for merge
+- merge file function
+- upload file to ecm
--- src/db/schema/mod.rs
+++ src/db/schema/mod.rs
@@ -1,10 +1,12 @@
+// @generated automatically by Diesel CLI.
+
 diesel::table! {
     users (id) {
-        id         -> Int4,
-        username   -> Varchar,
-        email      -> Varchar,
-        full_name  -> Nullable<Varchar>,
-        active     -> Bool,
+        id -> Int4,
+        username -> Varchar,
+        email -> Varchar,
+        full_name -> Nullable<Varchar>,
+        active -> Bool,
         created_at -> Timestamp,
         updated_at -> Timestamp,
     }
